d2a87d4aeb9742bac00e49605adda94a6aa4cac3
Danny Garcia <dannygarcia.me@gmail.com>
2014-12-26 01:04:25 -0500

    Add OAuth callback URL & controller action #create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,7 @@ class SessionsController < ApplicationController
   def new
 
   end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
 
+  get "/auth/:provider/callback" => "sessions#create"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
