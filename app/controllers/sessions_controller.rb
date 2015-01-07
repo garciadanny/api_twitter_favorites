05188f8e967f12261cf48eccb2df40ae6ae98f18
garciadanny <dannygarcia.me@gmail.com>
2015-01-07 12:25:47 -0700

    Grab user from successful OAuth authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,14 @@
 class SessionsController < ApplicationController
 
   def new
-
   end
 
   def create
+    auth = request.env["omniauth.auth"]
+    user = User.find_by(twitter_id: auth['uid']) || User.create_user(auth)
+    render nothing: true # Will need to Redirect user to Ember app
+    # TODO
+    # Set up background workers to start getting favorites
+    # Create Session
   end
 end
