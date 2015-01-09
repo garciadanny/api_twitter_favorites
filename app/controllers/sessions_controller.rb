1036c13ed9e40f5d4086e49938e8cef906774558
garciadanny <dannygarcia.me@gmail.com>
2015-01-08 17:13:00 -0700

    #update_token
    Update the user’s access_token and access_token_secret on account
    creation and every time they log in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,7 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by(twitter_id: auth['uid']) || User.create_user(auth)
+    user.update_token( auth )
     render nothing: true # Will need to Redirect user to Ember app
-    # TODO
-    # Set up background workers to start getting favorites
-    # Create Session
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,12 @@ class User < ActiveRecord::Base
     create! do |user|
       user.twitter_id = auth['uid']
       user.twitter_handle = auth['info']['nickname']
-      user.access_token = auth['credentials']['token']
-      user.access_token_secret = auth['credentials']['secret']
       user.initial_favorites_count = auth['extra']['raw_info']['favourites_count']
     end
+  end
+
+  def update_token auth
+    self.update access_token: auth['credentials']['token']
+    self.update access_token_secret: auth['credentials']['secret']
   end
 end
