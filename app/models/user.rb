645037641b86cd2730c6e8124d23892b6f4a8aac
garciadanny <dannygarcia.me@gmail.com>
2015-01-08 17:43:22 -0700

    #fetch_favorites Fetch a user's favorited tweets
    #fetch_favorites takes a starting_point param, giving us the option of
    picking up where we left off by not fetching tweets we already have.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,24 @@ class User < ActiveRecord::Base
     self.update access_token: auth['credentials']['token']
     self.update access_token_secret: auth['credentials']['secret']
   end
+
+  def fetch_favorites options={}
+    starting_point = options[:starting_point]
+    if starting_point
+      client.favorites( count: 200, max_id: starting_point )
+    else
+      client.favorites( count: 200 )
+    end
+  end
+
+  private
+
+  def client
+    @client ||= Twitter::REST::Client.new do |c|
+      c.consumer_key = ENV['TWITTER_FAVORITES_KEY']
+      c.consumer_secret = ENV['TWITTER_FAVORITES_SECRET']
+      c.access_token = access_token # need to encrypt in db
+      c.access_token_secret = access_token_secret # need to encrypt in db
+    end
+  end
 end
