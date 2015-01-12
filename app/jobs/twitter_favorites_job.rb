b5c357ef740bbc96843e9fcf0161118d7154f6ab
garciadanny <dannygarcia.me@gmail.com>
2015-01-12 16:57:35 -0700

    TwitterFavoritesJob Background Worker
    The background worker is responsible for starting the runner and
    enqueuing another job if there are remaining tweets to grab from the API

--- app/jobs/twitter_favorites_job.rb
+++ app/jobs/twitter_favorites_job.rb
@@ -1,12 +1,17 @@
 class TwitterFavoritesJob < ActiveJob::Base
   queue_as :default
 
-  after_perform do |job|
-    # this will get called after #perform has finished
-    # Check if there's more tweets to get and if so, call #perform 15 minutes from now
+  def perform(runner_class, user)
+    favorites_runner = runner_class.constantize.new( user )
+    favorites_runner.start
+    enqueue_next_job user
   end
 
-  def perform(*args)
-    # Do something later
+  def enqueue_next_job user
+    unless user.last_fetched_favorite.last_favorite?
+      TwitterFavoritesJob.set(wait: 15.minutes).perform_later( 'FavoritesRunner', user )
+    end
   end
 end
+
+
--- spec/jobs/twitter_favorites_job_spec.rb
+++ spec/jobs/twitter_favorites_job_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
-RSpec.describe TwitterFavoritesJob, :type => :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe TwitterFavoritesJob do
+  include ActiveJob::TestHelper
+
+  let(:user) { User.create }
+
+  let(:favorites) do
+    [Twitter::Tweet.new(id: 1), Twitter::Tweet.new(id: 2)]
+  end
+
+  before do
+    allow_any_instance_of(FavoritesRunner).to receive(:fetch).and_return favorites
+  end
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
+
+  describe '#perform' do
+
+    it "enqueue's a job to be performed when the queue is free" do
+        TwitterFavoritesJob.perform_later( 'FavoritesRunner', user)
+        expect(enqueued_jobs.size).to eq 1
+    end
+  end
+
+  describe '#enqueue_next_job' do
+
+    context 'when there are remaining favorited tweets' do
+
+      before do
+        # The first time #enqueue_next_job is called, #last_favorite? will return false
+        # indicating there are remainging tweets and to enqueue another job.
+        # The second time #enqueue_next_job is called, it'll return true
+        # indicating there are no more remainging tweets and another job does not need to be queued.
+        # This will avoid a "stack level too deep" error in our test
+        allow(user.last_fetched_favorite).to receive(:last_favorite?).and_return(false, true)
+      end
+
+      it "performs another job for a total of 2 jobs" do
+        perform_enqueued_jobs do
+          TwitterFavoritesJob.perform_later( 'FavoritesRunner', user)
+          expect(performed_jobs.size).to eq 2
+        end
+      end
+    end
+
+    context 'when there are no more remainging tweets' do
+      it 'performs a total of one job' do
+        perform_enqueued_jobs do
+          TwitterFavoritesJob.perform_later( 'FavoritesRunner', user)
+          expect(performed_jobs.size).to eq 1
+        end
+      end
+    end
+  end
 end
