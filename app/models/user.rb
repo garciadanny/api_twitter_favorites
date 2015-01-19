b245caf64815bbf63584b09fd16b8f096f3fbc27
Danny Garcia <dannygarcia.me@gmail.com>
2015-01-18 17:35:23 -0700

    build User#last_fetched_favorite on user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,6 @@ class User < ActiveRecord::Base
   has_one :last_fetched_favorite, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  after_initialize do
-    last_fetched_favorite ||= build_last_fetched_favorite
-  end
-
   def self.create_user auth
     create! do |user|
       user.twitter_id = auth['uid']
@@ -13,6 +9,7 @@ class User < ActiveRecord::Base
       user.initial_favorites_count = auth['extra']['raw_info']['favourites_count']
       user.access_token = auth['credentials']['token']
       user.access_token_secret = auth['credentials']['secret']
+      user.build_last_fetched_favorite
     end
   end
 
