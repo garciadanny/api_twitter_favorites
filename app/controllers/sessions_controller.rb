c80d1c70eea5b77741f26b7520ad9ce1207c7ab8
Danny Garcia <dannygarcia.me@gmail.com>
2015-01-24 22:38:16 -0700

    Workers only need a user to start jobs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by(twitter_id: auth['uid']) || User.create_user(auth)
-    TwitterFavoritesJob.perform_later( 'FavoritesRunner', user )
+    TwitterFavoritesJob.perform_later( user )
     render nothing: true # Will need to Redirect user to Ember app, and tell Ember if it's a new user.
   end
 end
