6246ad3deec1c70186b5726d0f7de2b7ba9d16bb
Danny Garcia <dannygarcia.me@gmail.com>
2015-02-12 20:29:12 -0700

    remove comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,6 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by(twitter_id: auth['uid']) || User.create_user(auth)
     TwitterFavoritesJob.perform_later( user )
-    render nothing: true # Will need to Redirect user to Ember app, and tell Ember if it's a new user.
+    render nothing: true
   end
 end
